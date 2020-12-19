2377bc19b3884783ae699f3faa056bcd5f501ed7
Mohit Patni <mohitpatni0731@gmail.com>
2020-12-19 21:16:55 +0530

    Add files via upload

---
+++ numberguessing.py
@@ -0,0 +1,28 @@
+import random, string
+
+MAX_LIMIT = 100
+n = random.randint(1, MAX_LIMIT)
+
+def get_input():
+    guess = None
+    while guess == None:
+        try:
+            guess = float(input("Enter an integer from 1 to %s: " % MAX_LIMIT))
+            return guess
+        except:
+            continue
+
+guess = get_input()
+while n != "guess":
+    print
+    if guess < n:
+        print ("guess is low")
+        guess = get_input()
+    elif guess > n:
+        print ("guess is high")
+        guess = get_input()
+    else:
+        print ("you guessed it!")
+        break
+    print
+print ("Congratulations")
